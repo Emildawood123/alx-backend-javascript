e28e6267ac56f0b59baed2cf20271ed2cbec90ef
emil <edawood997@gmail.com>
2024-03-04 19:40:46 +0200

    five

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const arr3 = new Array(...array1, ...array2, ...string.split(''));
+  const arr3 = [...array1, ...array2, ...string.split('')];
   return arr3;
 }
