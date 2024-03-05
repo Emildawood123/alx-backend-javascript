28e27925ab46f5ddbccb364d8c897d018fe161f3
Emildawood123 <edawood997@gmail.com>
2024-03-05 10:34:22 -0500

    done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,9 +2,15 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const upload = (await uploadPhoto()).status === 200 ? await uploadPhoto() : null;
-  const use = (await createUser()).status === 200 ? await createUser() : null;
-  return ({
-    photo: upload,
-    user: use,
-  });
+  const use = await createUser();
+  if (upload === null) {
+    return ({
+      photo: upload,
+      user: use,
+    });
+  }
+  return {
+    photo: null,
+    user: null,
+  };
 }
