c086fdc88cbcbb11e3823ea96c8607f843dc8c12
Emildawood123 <edawood997@gmail.com>
2024-03-06 04:33:07 -0500

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((res) => createUser().then((resolve) => {
-    console.log(`${res.body} ${resolve.firstName} ${resolve.lastName}`);
-  }).catch(() => console.log('Signup system offline'))).catch(() => console.log('Signup system offline'));
+  try {
+    Promise.all([uploadPhoto(), createUser()])
+      .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`));
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
