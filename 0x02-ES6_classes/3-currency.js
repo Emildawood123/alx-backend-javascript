6bbc1186939e1526feea54bc775f93acbe4c1a98
Emildawood123 <edawood997@gmail.com>
2024-03-07 10:36:44 -0500

    3 edit

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,4 +10,20 @@ export default class Currency {
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
 }
