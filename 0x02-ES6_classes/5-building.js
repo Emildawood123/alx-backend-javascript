74c58c1a42dceffc25a7bac7236f3c24648ff4a7
Emildawood123 <edawood997@gmail.com>
2024-03-07 11:07:55 -0500

    OOOOk

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,20 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
-  get getSqft() {
+  get sqft() {
     return this._sqft;
   }
 
-  set setSqft(newSqft) {
+  set sqft(newSqft) {
     this._sqft = newSqft;
   }
 }
