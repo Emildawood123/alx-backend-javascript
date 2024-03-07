50acfc6a671e7b27470f7cbe459bd7be3a6917ce
Emildawood123 <edawood997@gmail.com>
2024-03-07 10:16:13 -0500

    jokkjlga

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -32,6 +32,9 @@ export default class HolbertonCourse {
   }
 
   set setLength(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Name must be a number');
+    }
     this._name = newLength;
   }
 
@@ -42,4 +45,22 @@ export default class HolbertonCourse {
   set setStudents(newStudents) {
     this._name = newStudents;
   }
+}
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+console.log(c1.name);
+c1.name = "Python 101";
+console.log(c1);
+
+try {
+    c1.name = 12;
+} 
+catch(err) {
+    console.log(err);
+}
+
+try {
+    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+}
+catch(err) {
+    console.log(err);
 }
