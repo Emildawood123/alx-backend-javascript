c3f70818cd52847e5bb5eef048e59a4ffc7f0884
Emildawood123 <edawood997@gmail.com>
2024-03-12 06:17:48 -0400

    extra edit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,13 @@
 export default function cleanSet(set, startString) {
   const res = [];
+  if (startString === '') {
+    return '';
+  }
   for (const item of set) {
-    if (startString === '') {
-      return '';
-    } if (item.startsWith(startString)) {
+    if (item.startsWith(startString)) {
       const newItem = item.substring(startString.length);
       res.push(newItem);
     }
-  }
-  if (res.length === 0) {
-    return '';
   }
   return res.join('-');
 }
