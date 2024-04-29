7697f8158eedef5312ac4acff6fce88ed48a1f35
Emildawood123 <edawood997@gmail.com>
2024-04-29 11:34:35 -0400

    Okey

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,22 +3,14 @@ const readline = require('readline');
 
 function countStudents(path) {
   const readStream = fs.createReadStream(path);
-
-  // Create a readline interface
   const readInterface = readline.createInterface({
     input: readStream
-  });
-
-  // Initialize an array to store the parsed data
+  })
   const output = [];
-
-  // Event handler for reading lines
   readInterface.on('line', (line) => {
     const row = line.split(',');
     output.push(row);
   });
-
-  // Event handler for the end of file
   readInterface.on('close', () => {
     const numberOfStudent = [];
     for (const i of output) {
@@ -40,8 +32,6 @@ function countStudents(path) {
       console.log(`Number of students in ${key}: ${obj[key].count}. List: ${obj[key].lst.join(', ')}`);
     }
   });
-
-  // Event handler for handling errors
   readInterface.on('error', () => {
     throw new Error('Cannot load the database');
   });
