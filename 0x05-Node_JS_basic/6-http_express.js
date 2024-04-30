5c6e2a66efde839df28ebb7906908c82a322a54c
Emildawood123 <edawood997@gmail.com>
2024-04-30 06:39:21 -0400

    it's simple

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,9 @@
+const express = require('express', 1);
+const port = 1245;
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.listen(port, () => {
+  console.log(`app works in prot ${port}`);
+});
