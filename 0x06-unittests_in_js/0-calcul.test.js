f3166bbd6d7134fd2ff2dd847638bf5814c025f4
Emildawood123 <edawood997@gmail.com>
2024-05-01 04:46:43 -0400

    strict equal instead of basic equial

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,15 +4,15 @@ const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', function () {
     it('should return add after rounded', function () {
-      assert.equal(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
     it('should return add after rounded', function () {
-      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
     it('should return add after rounded', function () {
-      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
     it('should return add after rounded', function () {
-      assert.equal(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
