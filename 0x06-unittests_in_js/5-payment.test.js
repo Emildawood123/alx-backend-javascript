f8fae148e8fb4ca401cc1caa1d20f956695c9053
Emildawood123 <edawood997@gmail.com>
2024-05-01 14:30:23 -0400

    sure 100%

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -9,12 +9,12 @@ describe('sendPaymentRequestToApi', () => {
     afterEach(() => {
         consoleSpy.restore();
     })
-  it('should call calculateNumber', () => {
+  it('should call with 100, 20', () => {
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWithMatch(consoleSpy, 'The total is: 10');
     sinon.assert.calledOnce(calculateNumberS, 'SUM', 100, 20);
   });
-  it('should call calculateNumber', () => {
+  it('should call with 10, 10', () => {
     sendPaymentRequestToApi(10, 10);
     sinon.assert.calledWithMatch(consoleSpy, 'The total is: 10');
     sinon.assert.calledOnce(calculateNumberS, 'SUM', 10, 10);
