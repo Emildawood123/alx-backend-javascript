aff237d6fcab1aed4ff98f8e28561881c507b554
Emildawood123 <edawood997@gmail.com>
2024-05-01 05:09:46 -0400

    Okey

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,6 @@
+const calculateNumber = require('./2-calcul_chai');
+const { expect } = require('chai');
+expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
+expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
+expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error')
