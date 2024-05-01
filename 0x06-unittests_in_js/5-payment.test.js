d5643d804342f249fd0e4f5b2024d28008af5d6a
Emildawood123 <edawood997@gmail.com>
2024-05-01 14:28:53 -0400

    sure 100%

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -2,7 +2,6 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./5-payment');
 describe('sendPaymentRequestToApi', () => {
-  it('should call calculateNumber', () => {
     let consoleSpy;
     beforeEach(() => {
         consoleSpy = sinon.spy(console, 'log');
@@ -10,11 +9,14 @@ describe('sendPaymentRequestToApi', () => {
     afterEach(() => {
         consoleSpy.restore();
     })
+  it('should call calculateNumber', () => {
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWithMatch(consoleSpy, 'The total is: 10');
-    sinon.assert.calledWithMatch(calculateNumberS, 'SUM', 100, 20);
+    sinon.assert.calledOnce(calculateNumberS, 'SUM', 100, 20);
+  });
+  it('should call calculateNumber', () => {
     sendPaymentRequestToApi(10, 10);
     sinon.assert.calledWithMatch(consoleSpy, 'The total is: 10');
-    sinon.assert.calledWithMatch(calculateNumberS, 'SUM', 10, 10);
+    sinon.assert.calledOnce(calculateNumberS, 'SUM', 10, 10);
   });
 });
