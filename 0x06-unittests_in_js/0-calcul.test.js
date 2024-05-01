c28691960631a20abd984a3db73dddc40f39da7b
Emildawood123 <edawood997@gmail.com>
2024-05-01 04:44:43 -0400

    without describe inside describe

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,24 +3,16 @@ const calculateNumber = require('./0-calcul')
 
 
 describe('calculateNumber', function () {
-  describe('2 integer', function () {
     it('should return add after rounded', function () {
       assert.equal(calculateNumber(1, 3), 4);
     });
-  });
-  describe('first intger and second float', function () {
     it('should return add after rounded', function () {
       assert.equal(calculateNumber(1, 3.7), 5);
     });
-  });
-  describe('first float for floor and other float to ceil', function () {
     it('should return add after rounded', function () {
       assert.equal(calculateNumber(1.2, 3.7), 5);
     });
-  });
-  describe('2 float to ceil', function () {
     it('should return add after rounded', function () {
       assert.equal(calculateNumber(1.5, 3.7), 6);
     });
-  });
 });
