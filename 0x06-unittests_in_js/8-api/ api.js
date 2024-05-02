29983a4ada7d0b23aca5879fde36b9ffa96974dc
Emildawood123 <edawood997@gmail.com>
2024-05-02 04:14:03 -0400

    confused in request in

--- 0x06-unittests_in_js/8-api/ api.js
+++ 0x06-unittests_in_js/8-api/ api.js
@@ -1,10 +1,9 @@
-const Express = require('express')
+const express = require('express')
 
-const app = Express()
+const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system')
 })
 app.listen(7865, () => {
     console.log('API available on localhost port 7865')
 })
-module.exports = app
